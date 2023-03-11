22d67fb6b9f2088d9617f41532daa98bdff088dd
unknown <didin.jamaludin@ifg-life.id>
2023-03-11 09:43:56 +0700

    add cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const products = require('./routes/products');
+const cors = require('cors');
 
 mongoose.Promise = global.Promise;
 
@@ -21,6 +22,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/products', products);
+app.use('/api/v1/products', products);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
